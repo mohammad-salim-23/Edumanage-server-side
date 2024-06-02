a2756e05425d4bbf122fe24cdec5fca76c024b1e
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-06-01 23:35:09 -0700

    userCollection create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipsrkdy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -16,24 +16,52 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db("EduManage").collection("users");
+   
+
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("jwt...", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // user related api
+    app.post('/users',async(req,res)=>{
+      const user = req.bosy;
+      // insert email id users doesn't exist
+      const query = {email:user.enail};
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        console.log(existingUser);
+        return res.send({message:'user already exists',insertedId:null})
+      }
+      const result = userCollection.insertOne(user);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-app.get("/",(req,res)=>{
-    res.send("EduManage is running");
-})
-app.listen(port,()=>{
-    console.log(`EduManage is running on port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("EduManage is running");
+});
+app.listen(port, () => {
+  console.log(`EduManage is running on port ${port}`);
+});
