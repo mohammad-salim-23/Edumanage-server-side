61e1989fd9d0e80108360da87026268a7662d7aa
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-06-02 05:26:06 -0700

    VerifyToken create

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipsrkdy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +23,22 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const userCollection = client.db("EduManage").collection("users");
-   
+  //  created middleware
+  const verifyToken = (req,res,next)=>{
+    console.log("inside verify Token",req.headers);
+    if(!req.headers.authorization){
+      return res.status(401).send({message:'unauthorized access'})
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){
+        return res.status(401).send({message:'unauthorized access'})
+      }
+      req.decoded = decoded;
+      next();
+     })
+    
+  }
 
     // jwt related api
     app.post("/jwt", async (req, res) => {
@@ -37,15 +52,26 @@ async function run() {
 
     // user related api
     app.post('/users',async(req,res)=>{
-      const user = req.bosy;
+      const user = req.body;
       // insert email id users doesn't exist
-      const query = {email:user.enail};
+      const query = {email:user.email};
       const existingUser = await userCollection.findOne(query);
       if(existingUser){
         console.log(existingUser);
         return res.send({message:'user already exists',insertedId:null})
       }
       const result = userCollection.insertOne(user);
+      res.send(result);
+    })
+    app.patch('/users/admin/:id',verifyToken,async(req,res)=>{
+      const id = req.params.id;
+      const filter={_id:new ObjectId(id)};
+      const updateDoc = {
+        $set:{
+          role:'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updateDoc);
       res.send(result);
     })
     // Send a ping to confirm a successful connection
